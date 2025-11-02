1a797a496546ed1ecca3dac88234f2395237d7c1
Christian Yana <cyanahu@unsa.edu.pe>
2025-11-02 12:04:48 -0500

    se cambia el lenguaje java a c++ ejercicio 1

---
+++ Ejercicio_1/Fibonacci.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector> // Para usar std::vector
+
+using namespace std;
+
+int fibonacci(int n) {
+    if (n < 0) {
+        cout << "Error: la posicion no puede ser negativa." << endl;
+        return -1;
+    }
+
+    if (n <= 1) {
+        return n;
+    }
+
+    vector<int> tabla(n + 1);
+    tabla[0] = 0;
+    tabla[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        tabla[i] = tabla[i - 1] + tabla[i - 2];
+    }
+
+    return tabla[n];
+}
+
+int main() {
+    int n = 3;
+    cout << "Fibonacci de la posicion " << n << " es: " << fibonacci(n) << endl;
+    return 0;
+}
